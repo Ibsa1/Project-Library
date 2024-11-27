14ae507f2749f4a3079f7edb1aefd502337f8e3c
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-11-27 16:26:08 +0300

    Book Object constructor function

---
+++ script.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+/*
+
+function Book(author, title, pages, read){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function(){
+        return (title + " by " + author + " " + pages + " " + "pages " + read);
+    };
+}
+const BookOne = new Book("Ibsa Milkeso", "thestory", 250, "not read");
+
+BookOne.info();
+
+*/
